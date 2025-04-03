dca84b70c929c3760e36c6de6b71976553014ec1
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-03 18:26:43 +0530

    java: blockchain -> main

--- java/main.java
+++ java/main.java
@@ -1,9 +1,5 @@
-import java.security.MessageDigest;
-import java.sql.Date;
-import java.util.ArrayList;
 
-/**
- * @file blockchain.java
+/** * @file blockchain.java
  * @author Krisna Pranav
  * @version 1.0
  * @date 2025-04-01
@@ -11,6 +7,10 @@ import java.util.ArrayList;
  * @copyright Copyright (c) 2025 Krisna Pranav
  * 
  */
+
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Date;
 
 class Block {
     public int index;
@@ -31,15 +31,13 @@ class Block {
 
     public String calculateHash() {
         try {
-            String input = index + previousHash + transactions + timestamp;
+            String input = index + previousHash + transactions + timestamp + nonce;
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hashBytes = digest.digest(input.getBytes());
             StringBuilder hexString = new StringBuilder();
-
             for (byte b : hashBytes) {
                 hexString.append(String.format("%02x", b));
             }
-
             return hexString.toString();
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -47,7 +45,11 @@ class Block {
     }
 
     public void mineBlock(int difficulty) {
-
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
     }
 }
 
@@ -62,10 +64,26 @@ class Blockchain {
     }
 
     public void addBlock(String transactions) {
-        Block newBlock = new Block(chain.size(), chain.get());
+        Block newBlock = new Block(chain.size(), chain.get(chain.size() - 1).hash, transactions);
+        newBlock.mineBlock(difficulty);
+        chain.add(newBlock);
     }
 
     public void printChain() {
+        for (Block block : chain) {
+            System.out.println("Index: " + block.index);
+            System.out.println("Hash: " + block.hash);
+            System.out.println("Previous Hash: " + block.previousHash);
+            System.out.println();
+        }
+    }
+}
 
+public class main {
+    public static void main(String[] args) {
+        Blockchain blockchain = new Blockchain(2);
+        blockchain.addBlock("Alice pays Bob 10 BTC");
+        blockchain.addBlock("Bob pays Charlie 5 BTC");
+        blockchain.printChain();
     }
 }
